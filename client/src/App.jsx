e58e3fecab8936fbc2cc5d5495345b724ae5a17b
Akshay7V <akshayv.ramakrishnan@gmail.com>
2024-04-15 14:06:36 +0530

    Home route created, home page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signin from './components/signin.jsx'
 import Signup from './components/signup.jsx'
+import Home from './pages/Home.jsx'
 
 export default function App() {
   return (
     <div className='h-screen w-screen'>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Home/>} />
           <Route path='/signup' element={<Signup/>} />
           <Route path='/signin' element={<Signin/>} />
         </Routes>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-3xl text-center'>Home Page</div>
+  )
+}
