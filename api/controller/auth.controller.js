01d124c315def9ca533cb4d17c1efe91e29ee8c4
Thamir <thamirsiddik@gmail.com>
2024-04-16 20:28:47 +0530

    Added signup functionality, Enhanced security measures for signin and signup routes.

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,37 +1,51 @@
-
 import { User } from "../models/user.model.js";
+
 export const signUp = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const existuser = await User.findOne({ email });
-        if (existuser) {
-            res.json({ message: 'User already existing!' });
-        } else {
-            const newUser = await User.create({
-                email,
-                password
-            });
-            res.json({ message: "New User Created Successfully", newUser });
-        }
-    } catch (error) {
-        res.json({ message: error.message });
+  try {
+    const { email, password } = req.body;
+    if (!email && !password) {
+      res.json({ message: "Email and Password is required" });
+    } else if (!email) {
+      res.json({ message: "Email is required" });
+    } else if (!password) {
+      res.json({ message: "Password is required" });
+    } else {
+      const existuser = await User.findOne({ email });
+      if (existuser) {
+        res.json({ message: "User already existing!" });
+      } else {
+        const newUser = await User.create({
+          email,
+          password,
+        });
+        const { email: mail, _id } = newUser._doc;
+        res.json({ mail, id: _id });
+      }
     }
-}
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
 
 export const signIn = async (req, res) => {
-    try {
-        const data = req.body;
-        const user = await User.findOne({ email: data.email });
-        if (user) {
-            if (user.password == data.password) {
-                res.json({ message: "User Found", user });
-            } else {
-                res.json({ message: "incorrect passeord" });
-            }
-        } else {
-            res.json({ message: "user not found " });
-        }
-    } catch (error) {
-        res.json({ error: error.message });
+  try {
+    const { email, password } = req.body;
+    if (!email && !password) {
+      res.json({ message: "Email and Password is required" });
+    } else if (!email) {
+      res.json({ message: "Email is required" });
+    } else if (!password) {
+      res.json({ message: "Password is required" });
+    } else {
+      if (!email && !password) {
+        res.json({ message: "Email and Password is required" });
+      } else if (!email) {
+        res.json({ message: "Email is required" });
+      } else if (!password) {
+        res.json({ message: "Password is required" });
+      }
     }
-}
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/user/signup", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+      });
+      if (res.ok) {
+        const data = res.json();
+      } else {
+        console.error("Error ", await res.json());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='h-96 w-72 bg-blue-200 flex flex-col gap-2 mt-24 mx-auto rounded-xl p-4'>
-      <h2 className='text-2xl font-semibold text-center'>Create Your Account</h2>
-      <input className='w-full h-10 p-2 border-2 border-gray-500 rounded-md' type="email" placeholder='Email'/>
-      <input className='w-full h-10 p-2 border-2 border-gray-500 rounded-md' type="password" placeholder='Password'/>
-      <button className='w-full py-2 px-1 bg-black text-white rounded-md' type='submit'>Sign Up</button>
-      <p className='text-center'>Already have an account? <Link className='font-semibold' to={'/signin'}>Sign In</Link></p>
+    <div className="h-96 w-72 bg-blue-200 flex flex-col gap-2 mt-24 mx-auto rounded-xl p-4">
+      <h2 className="text-2xl font-semibold text-center">
+        Create Your Account
+      </h2>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+        <input
+          className="w-full h-10 p-2 border-2 border-gray-500 rounded-md"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="w-full h-10 p-2 border-2 border-gray-500 rounded-md"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="w-full py-2 px-1 bg-black text-white rounded-md"
+          type="submit"
+        >
+          Sign Up
+        </button>
+      </form>
+      <p className="text-center">
+        Already have an account?{" "}
+        <Link className="font-semibold" to={"/signin"}>
+          Sign In
+        </Link>
+      </p>
     </div>
   );
 }
