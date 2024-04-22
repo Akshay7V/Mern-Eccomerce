265bfcb8e96d0978761ec6d9b13808450a9d25ec
Akshay7V <akshayv.ramakrishnan@gmail.com>
2024-04-22 12:53:18 +0530

    Get all products route for users created.

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { signIn, signUp } from "../controller/auth.controller.js";
+import { getAllProducts } from "../controller/user.controller.js";
 
 const app = express();
 
 app.post('/signup', signUp);
 app.post('/signIn', signIn);
+app.get('/get-products', getAllProducts);
 
 export default app;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const res = await fetch('/api/user/get-products');
+        if (res.ok) {
+          const products = await res.json();
+          setProducts(products)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAllProducts();
+  }, [])
+
+  console.log(products);
+
   return (
     <div className="">
       <Header />
